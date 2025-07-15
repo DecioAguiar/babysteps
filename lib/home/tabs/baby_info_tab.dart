c9ef29d87a6ea0b37b90864de1269a420cc59b13
Ruan Queiroz <ruanqueirozdev@gmail.com>
2025-07-15 13:59:23 -0300

    alteracoes na home screen, ajuste no plano de fundo

--- lib/home/tabs/baby_info_tab.dart
+++ lib/home/tabs/baby_info_tab.dart
@@ -1,6 +1,5 @@
 // info do bebe
 import 'package:flutter/material.dart';
-import 'package:babysteps/core/theme.dart';
 
 class BabyInfoTab extends StatelessWidget {
   const BabyInfoTab({super.key});
